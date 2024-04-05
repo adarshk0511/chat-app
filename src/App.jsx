3f589c07b48e76e1731f285567e2bc1b850f04a2
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-05 20:47:54 +0530

    created public and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,30 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
+  // return (
+  //   <BrowserRouter>
+  //     <Switch>
+  //       <Route path="/signin">SignIn</Route>
+  //       <Route path="/">Home</Route>
+  //     </Switch>
+  //   </BrowserRouter>
+  // );
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 

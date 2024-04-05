74aec6019ec7280ae7d9a30565cc04cc596a9f64
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-06 01:36:09 +0530

    manage user with profile

---
+++ src/context/profile.context.js
@@ -0,0 +1,64 @@
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import { auth, database } from "../misc/firebase";
+
+
+const ProfileContext = createContext();
+
+export const profileProvider = ({childern}) => {
+
+    let userRef;
+
+    const [profile, setProfile] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+      const authUnsub =  auth.onAuthStateChanged(authObj => {
+            if(authObj){
+
+                userRef = database.ref(`/profiles/${authObj.uid}`);
+                userRef.on('value', (snap)=>{
+                    const {name, createdAt} = snap.val();
+                    
+                    const data = {
+                        name,
+                        createdAt,
+                        uid : authObj.uid,
+                        email : authObj.email,
+    
+                    };
+                    setProfile(data);
+                    isLoading(false);
+                });
+
+                
+            }
+            else{
+
+                if(userRef){
+                    useRef.off();
+                }
+
+                setProfile(null);
+                isLoading(false);
+            }
+        });
+        return () => {
+            authUnsub();
+
+            if(userRef){
+                userRef.off();
+            }
+        }
+    },[]); 
+
+
+    return (
+        <ProfileContext.Provider value={{isLoading, profile}}>
+        {childern}
+    </ProfileContext.Provider>
+    )
+    
+}
+
+export const useProfile = () => useContext(ProfileContext);

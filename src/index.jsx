01ba612c64d158487519a2b1205306869f9480a6
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-06 00:03:04 +0530

    added sign in page for facebook and google

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,29 @@
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(<App />);
+
+
+// // eslint-disable-next-line react/no-deprecated
+// ReactDOM.render(
+//     <BrowserRouter>
+//     <App/>
+//     </BrowserRouter>,
+//     document.getElementById('root')
+// );
+
+
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>,
-    document.getElementById('root')
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 const config = {
   apiKey: 'AIzaSyABIJ3ITUrYLIixY43xu07PM7ZCBgnz2Ww',
-
   authDomain: 'chat-web-app-df8a3.firebaseapp.com',
-
   projectId: 'chat-web-app-df8a3',
-
   storageBucket: 'chat-web-app-df8a3.appspot.com',
-
   messagingSenderId: '907805806187',
-
   appId: '1:907805806187:web:6b90d3d176a43151326b2e',
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
 

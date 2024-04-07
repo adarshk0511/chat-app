5dc6526930d303d533a719d4ef5d9debff582967
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-08 01:35:19 +0530

    chat room list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import {Divider, useRef} from 'rsuite';
+import React, { useEffect, useState } from 'react'
 import DashboardToggle from './dashboard/DashboardToggle'
 import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal'
+import ChatRoomList from './rooms/ChatRoomList';
+
 const Sidebar = () => {
+
+  const TopSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(()=>{
+    if(TopSidebarRef.current)
+    setHeight(TopSidebarRef.current.scrollHeight  )
+  },[TopSidebarRef])
+
   return (
     <div className='h-100 pt-2'>
-        <div>
+        <div ref={TopSidebarRef}>
             <DashboardToggle/>
             <CreateRoomBtnModal/>
+            <Divider>Join conversation text</Divider>
         </div>
-        bottom
+        <ChatRoomList aboveElHeight={height}/>
     </div>
   )
 }
---
+++ src/context/rooms.context.js
@@ -0,0 +1,26 @@
+import { Children, createContext, useEffect, useState } from "react";
+import { database } from "../misc/firebase";
+import { transformToArrayWithId } from "../misc/helpers";
+
+
+const RoomsContext = createContext();
+
+
+export const RoomsProvider = ({children}) => {
+    const [rooms, setRooms] = useState(null);
+    useEffect(()=>{
+        const roomListRef = database.ref('rooms');
+
+        roomListRef.on('value', (snap)=> {
+            const data = transformToArrayWithId(snap.val())
+            setRooms(data);
+        })
+
+        return () =>{
+            roomListRef.off
+        }
+    },[])
+    return <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+}
+
+export const useRooms = () => useContext(RoomsContext); 
